77b53b71eb3a8ba52e42b8a73afdd5bdb3df5c08
adunofiur <matthew21573@gmail.com>
2016-02-26 15:10:51 -0600

    first commit yo

---
+++ pick/demo.js
@@ -0,0 +1,17 @@
+/*
+	Before the page loads, we create a bsService object. Do not change the name - that will break stuff :(
+		The arguments passed in are a list of strings. Each string is an extensionID. these particular ID's are
+		for the released version of the IdeaMACHE plugin and for a secret, special version of the extension 
+		that I have
+	bsService will see if any of the extenions are availabe. If so it will use them. Otherwise, it will rely on the
+		web-hosted version of bigsemantics
+
+	 */
+
+
+/*
+Called on the rendering demo page
+*/
+function onLoad(){
+
+}
